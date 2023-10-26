b188cdccfc3e04a746790243c641c574905ad6e7
Mario <mariodgweb@libero.it>
2023-10-26 17:47:59 +0200

    Il mio terzo commit

---
+++ esercitazioni.js
@@ -0,0 +1,51 @@
+// Esercizio 1
+console.log("ciaooooo")
+
+// Esercizio2
+let primoNum = 5;
+let secondoNum = 7;
+let terzoNum = 67;
+let quartoNum = 2;
+let quintoNum = 1;
+let somma = primoNum + secondoNum + terzoNum + quartoNum +quintoNum;
+let media = somma/5;
+console.log(`somma ${somma} media ${media}`)
+
+// esercizio3
+
+let str1 ='lawr';
+let str2 = "sve";
+let val1 = undefined;
+let val2 = null;
+let myNum = 1111;
+console.log(str1, typeof str1);
+console.log(str2, typeof str2);
+console.log(val1, typeof val1);
+console.log(val2, typeof val2);
+console.log(myNum, typeof myNum);
+
+// esercizio4
+
+let myName = "mario";
+let myAge = 21;
+let js = true;
+console.log(`Il mio nome ${myName} età ${myAge} , so programmare: ${js}`)
+
+// esercizio5
+
+let miles = 1762.921;
+let km = miles * 1.60934;
+console.log(`${miles}miles are equal to ${km} kms`)
+
+// esercizio6
+
+let inch = 72;
+let weight = 36;
+let altezza = inch*2.4;
+let altezzaMetri = altezza/100;
+let kg = weight * 2.2046;
+console.log(altezzaMetri, kg);
+let bmi = (kg/ (altezzaMetri ** 2));
+console.log("bmi", bmi);
+
+// esercizio7 
